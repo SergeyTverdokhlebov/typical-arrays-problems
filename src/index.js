f69a5d3730b8d1b548a87a6faa22b03513daf5f4
SergeyTverdokhlebov <sergey.tverdokhlebov@gmail.com>
2020-10-03 00:27:12 +0300

    feat: solve arrays problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,35 @@
 
 exports.min = function min (array) {
-  return 0;
-}
+  if (!array || array.length === 0) {
+    return 0;
+  }
+  else{
+    let min = 0;
+    min = Math.min.apply(null, array)
+    return min;
+    }
+  }
 
 exports.max = function max (array) {
-  return 0;
-}
+  if (!array || array.length === 0)  {
+    return 0;
+  }
+  else{
+    let max = 0;
+    max = Math.max.apply(null, array)
+    return max;
+    }
+  }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (!array || array.length === 0)  {
+    return 0;
+  }
+let summ=0;
+let avg=0;
+      for (var i = 0; i < array.length; i++) {
+        summ += array[i]
+    } 
+    avg=summ/array.length
+  return avg;
 }
